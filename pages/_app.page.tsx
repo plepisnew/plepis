a75b8b269ed514bbbf4161363fc7003df636a4f8
ansis <gusplean@student.gu.se>
2022-12-01 23:43:23 +0100

    Add _document

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -5,28 +5,19 @@ import type { AppProps } from 'next/app';
 import theme from '@/theme';
 import { ThemeProvider } from '@mui/material';
 import Header from '@/components/Header';
-import { Provider } from 'react-redux';
+import { Provider as StoreProvider } from 'react-redux';
 import store from '@/store';
-import Head from 'next/head';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Plepis Portfolio</title>
-      </Head>
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <Header />
-          <Page>
-            <Component {...pageProps} />
-          </Page>
-        </ThemeProvider>
-      </Provider>
-    </>
+    <StoreProvider store={store}>
+      <ThemeProvider theme={theme}>
+        <Header />
+        <Page>
+          <Component {...pageProps} />
+        </Page>
+      </ThemeProvider>
+    </StoreProvider>
   );
 };
 
--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import { Typography, Button, CircularProgress, Box } from '@mui/material';
+import { Typography } from '@mui/material';
 import RowStack from '@/components/auxiliary/RowStack';
-import useComboDrawer from './useComboDrawer';
 
 const HomePage: React.FC = () => {
   return (
